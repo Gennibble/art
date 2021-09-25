7b7de69e7df22a7e24733744a7b8a46e269b8cca
Gennibble <91382933+Gennibble@users.noreply.github.com>
2021-09-25 18:29:45 +0100

    initial purple circle collection
    Revert "initial purple circle collection"
    
    This reverts commit bfefe97c5602e0ef84ba7fb075a427dfaf7d10eb.

---
+++ libraries/GetVars.js
@@ -0,0 +1,12 @@
+function getParams() {
+    var idx = document.URL.indexOf('?');
+    var params = new Array();
+    if (idx != -1) {
+    var pairs = document.URL.substring(idx+1, document.URL.length).split('&');
+    for (var i=0; i<pairs.length; i++) {
+    nameVal = pairs[i].split('=');
+    params[nameVal[0]] = nameVal[1];
+       }
+    }
+    return params;
+}
